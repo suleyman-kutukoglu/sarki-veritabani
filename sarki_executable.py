99e4bbb3e161461342d08709d84ca180ddeebbf8
Suleyman <suleyman.kutukoglu@bil.omu.edu.tr>
2020-04-19 21:13:36 +0300

    Beta

--- sarki_executable.py
+++ sarki_executable.py
@@ -1,6 +1,6 @@
 from sarki_class_modul import *
 
-print("""***********************************
+bilgi_mesaji = """***********************************
 
 Şarkı Veritabanı Programına Hoşgeldiniz.
 
@@ -14,10 +14,12 @@ print("""***********************************
 
 4. Şarkı Sil
 
-6. Toplam Şarkı Süresini Göster
+5. Toplam Şarkı Süresini Göster
 
 Çıkmak için 'q' ya basın...
-***********************************""")
+***********************************"""
+
+print (bilgi_mesaji)
 
 sarki_kutuphanesi = Sarki_Kutuphanesi()
 
@@ -25,82 +27,103 @@ while True:
     işlem = input("Yapacağınız İşlem:")
 
     if (işlem == "q"):
+        print(bilgi_mesaji)
+        print("   "*3000)
         print("Program Sonlandırılıyor.....")
         print("Yine bekleriz....")
         break
     elif (işlem == "1"):
-        print("****************************")
+        print("   "*3000)
+        print(bilgi_mesaji)
         sarki_kutuphanesi.sarkilari_goster()
 
     elif (işlem == "2"):
+        print("   "*3000)
+        print(bilgi_mesaji)
         isim = input("Hangi sarkiyi istiyorsunuz ? ")
         print("Sarki Sorgulanıyor...")
-
 
         sarki_kutuphanesi.sarki_sorgula(isim)
 
     elif (işlem == "3"):
-        isim = input("İsim:")
-        tur = input("Tür:")
-        sanatci = input("Sanatçı:")
-        sure = input("Süre:")
-        format_kontrol = sure.split(":")
 
+        isim = input("İsim:")
+
+        tur = input("Tür:")
+
+        sanatci = input("Sanatçı:")
+
+        sure = input("Süre:")
+
+        # Burada herhangi bir stringi parçalarına ayıran bir fonksiyon kullanarak, kullanının sadece xy:bc formatında süre bilgisi girmesini sağlandı.
+        format_kontrol = sure.split(":")
         def ayirici(kelime):
             return [char for char in kelime]
 
         ayrilmis_liste = ayirici(sure)
 
-
-        sayac = 0
-        onay = False
+# ':' karakteri xx:ab formatında sadece bir kere geçmelidir. Bu yüzden bu for döngüsünde ':' karakterini sayıp
+        karakter_sayisi = 0
+        uygun = False
         for e in ayrilmis_liste:
             if len(ayrilmis_liste) == 5:
                 if e == ':':
-                    sayac += 1
+                    karakter_sayisi += 1
 
-                if sayac == 1:
-                    onay = True
+                if karakter_sayisi == 1:
+                    uygun = True
                 else:
-                    onay = False
+                    uygun = False
             else:
-                onay == False
+                uygun == False
 
-
-
-
-
-        if onay == True:
-            if len(format_kontrol) == 2:
+        if uygun == True:
+            if len(format_kontrol) == 2: # xx:ab formatında girilen bir süre değeri, split(':') fonksiyonuyla en fazla 2 elemana sahip bir diziye dönüşür.
                         try:
-                            format_kontrol[0] = int(format_kontrol[0])
+                            format_kontrol[0] = int(format_kontrol[0]) #Eğer kullanıcı sayı yerine harf, karakter vb girerse tam sayıya çevrilemeyen bir değer olduğundan hata almadan doğru format istenir.
                             format_kontrol[1] = int(format_kontrol[1])
 
                             if format_kontrol[1] <= 59 and format_kontrol[0] <= 59:
+                                print("   " * 3000)
+                                print(bilgi_mesaji)
                                 yeni_sarki = Sarki(isim, tur, sanatci, sure)
                                 print("Sarki ekleniyor....")
+                                print("")
                                 time.sleep(2)
                                 sarki_kutuphanesi.sarki_ekle(yeni_sarki)
                                 print("Sarki Eklendi....")
                         except ValueError:
-                            print("Hatalı format girdiniz XX:YY")
+                            print("   " * 3000)
+                            print(bilgi_mesaji)
+                            print("Hatalı format girdiniz. xy:ab şeklinde süre bilgisi girin.")
             else:
-                print("Hatalı Format")
+                print("   "*3000)
+                print(bilgi_mesaji)
+                print("Hatalı format girdiniz. xy:ab şeklinde süre bilgisi girin.")
         else:
-            print("Hatalı format girdiniz. Formatı XX:YY Şeklinde giriniz.")
+            print("   "*3000)
+            print(bilgi_mesaji)
+            print("Hatalı format girdiniz. xy:ab şeklinde süre bilgisi girin.")
+
+#Kullanıcının süre formatını kontrol eden işlemlerin sonu.
 
 
     elif (işlem == "4"):
+        print(bilgi_mesaji)
         isim = input("\nHangi şarkıyı silmek istiyorsunuz ?\n")
 
         cevap = input("Emin misiniz ? (E/H)\n")
         if (cevap == "E"):
-            time.sleep(2)
+            print("   "*3000)
+            print(bilgi_mesaji)
             sarki_kutuphanesi.sarki_sil(isim)
 
     elif (işlem == "5"):
-
+        print("   "*3000)
+        print(bilgi_mesaji)
         sarki_kutuphanesi.toplam_sure()
 
     else:
+        print("   "*3000)
+        print(bilgi_mesaji)
         print("Geçersiz İşlem...\n")
